246cc5325c0372f37c7621d8963ecd964c1aae37
opilane <opilane@HP-CZC2349HT2>
2024-02-26 13:35:01 +0200

    Picker ready

---
+++ Mobile/PickerPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static System.Net.WebRequestMethods;
+
+namespace Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickerPage : ContentPage
+    {
+        Picker picker;
+        WebView webView;
+        StackLayout st;
+        string[] lehed = new string[3] { "https://github.com/KiLcRaFt", "https://moodle.edu.ee/course/view.php?id=37973", "https://www.tthk.ee/" };
+        string[] nimetused = new string[3] { "Github", "Moodle", "TTHK" };
+        public PickerPage()
+        {
+            picker = new Picker
+            {
+                Title = "Veebilehed"
+            };
+            foreach (string leht in nimetused)
+            {
+                picker.Items.Add(leht);
+            }
+
+            webView = new WebView
+            {
+                Source = new UrlWebViewSource{ Url= lehed[0] },
+                HeightRequest = 600,
+                WidthRequest=100
+            };
+            picker.SelectedIndex= 0;
+            SwipeGestureRecognizer swipe_R = new SwipeGestureRecognizer
+            {
+                Direction=SwipeDirection.Right
+            };
+            swipe_R.Swiped += Swipe_Swiped;
+            SwipeGestureRecognizer swipe_L = new SwipeGestureRecognizer { Direction=SwipeDirection.Left };
+            swipe_L.Swiped += Swipe_L_Swiped;
+
+            picker.SelectedIndexChanged += Valime_leht_avamiseks;
+            st = new StackLayout
+            {
+                Children = { picker, webView}
+            };
+            st.GestureRecognizers.Add(swipe_R);
+            st.GestureRecognizers.Add(swipe_L);
+            Content = st;
+
+        }
+
+        private void Swipe_L_Swiped(object sender, SwipedEventArgs e)
+        {
+            if (picker.SelectedIndex < lehed.Length && picker.SelectedIndex >= 0)
+            {
+                picker.SelectedIndex += 1;
+                
+            }
+            else if (picker.SelectedIndex == lehed.Length)
+            {
+                picker.SelectedIndex = 0;
+            }
+            var url = lehed[picker.SelectedIndex];
+            webView.Source = new UrlWebViewSource { Url = url };
+        }
+
+        private void Swipe_Swiped(object sender, SwipedEventArgs e)
+        {
+            if (picker.SelectedIndex > 0 && picker.SelectedIndex <= lehed.Length)
+            {
+                picker.SelectedIndex -= 1;
+            }
+            else if(picker.SelectedIndex == 0)
+            {
+                picker.SelectedIndex = lehed.Length;
+            }
+            var url = lehed[picker.SelectedIndex];
+            webView.Source = new UrlWebViewSource { Url = url };
+        }
+
+        private void Valime_leht_avamiseks(object sender, EventArgs e)
+        {
+            webView.Source = new UrlWebViewSource { Url = lehed[picker.SelectedIndex] };
+        }
+    }
+}
--- Mobile/Startpage2_0.xaml.cs
+++ Mobile/Startpage2_0.xaml.cs
@@ -14,11 +14,11 @@ namespace Mobile
     {
             List<ContentPage> pages = new List<ContentPage>()
             {
-                new EntryPage(), new TimePage(), new BoxPage(), new lumememm(),  new FramePage(), new TripsTrapsTrullPage()
+                new EntryPage(), new TimePage(), new BoxPage(), new lumememm(),  new FramePage(), new TripsTrapsTrullPage(), new PickerPage()
             };
             List<string> texts = new List<string>()
             {
-                "Ava entry","Ava timer leht", "Ava Box leht", "Ava lumemmem leht", "Ava Frame leht", "Ava Trips Traps Trull leht"
+                "Ava entry","Ava timer leht", "Ava Box leht", "Ava lumemmem leht", "Ava Frame leht", "Ava Trips Traps Trull leht", "Ava Picker leht"
             };
             StackLayout st;
             public Startpage2_0()
