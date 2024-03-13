56a13b241a5371247d4a88e514a8d00fa01017c8
opilane <opilane@HP-CZC2349HTM>
2024-03-13 14:26:23 +0200

    tableView done

--- Mobile/Table_Page.xaml.cs
+++ Mobile/Table_Page.xaml.cs
@@ -4,8 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Plugin.Messaging;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Web;
+using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Mobile
 {
@@ -28,12 +32,16 @@ namespace Mobile
                 Text = "Saada sms",
                 WidthRequest = 120
             };
+            btn_sms.Clicked += Btn_sms_Clicked;
 
             btn_email = new Button
             {
                 Text = "Kirjuta",
                 WidthRequest = 120
             };
+            btn_email.Clicked += Btn_email_Clicked;
+
+
 
             tableview = new TableView
             {
@@ -80,24 +88,77 @@ namespace Mobile
                                 }
                             }
                         }
+                    },
+
+                    new TableSection("Tekst")
+                    {   
+                        new EntryCell
+                        {
+                            Placeholder = "Sisesta texti",
+                            Keyboard=Keyboard.Text
+                        }
                     }
                 }
             };
             Content = tableview;
         }
 
-        private void Btn_hel_Clicked(object sender, EventArgs e)
+        private async void Btn_email_Clicked(object sender, EventArgs e)
         {
-            var phoneNumberCell = tableview.Root
-            .FirstOrDefault(section => section.Title == "Kontaktiandmed:")
-            ?.FirstOrDefault(cell => (cell as EntryCell)?.Placeholder == "Sisesta tel. number") as EntryCell;
-            if (phoneNumberCell != null)
+            //var mail = CrossMessaging.Current.EmailMessenger;
+            //if (mail.CanSendEmail)
+            //{
+            //    mail.SendEmail(((EntryCell)tableview.Root[1][0]).Text, "Tervitus!", ((EntryCell)tableview.Root[3][0]).Text);
+            //}
+
+            try
             {
-                var call = CrossMessaging.Current.PhoneDialer;
-                if (call.CanMakePhoneCall)
+                var email = ((EntryCell)tableview.Root[1][1]).Text;
+                if (!string.IsNullOrWhiteSpace(email))
                 {
-                    call.MakePhoneCall(phoneNumberCell.Text);
-                };
+                    var subject = ((EntryCell)tableview.Root[0][0]).Text;
+                    var body = ((EntryCell)tableview.Root[3][0]).Text;
+                    var uri = new Uri($"mailto:{email}?subject={subject}&body={body}");
+                    await Launcher.OpenAsync(uri);
+
+                }
+                else
+                {
+                    await DisplayAlert("Viga", "palun sisesta email", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Viga", $"Viga emeiliga: {ex.Message}", "Ok");
+            }
+        }
+
+        private void Btn_sms_Clicked(object sender, EventArgs e)
+        {
+            var sms = CrossMessaging.Current.SmsMessenger;
+            if (sms.CanSendSms)
+            {
+                sms.SendSms(((EntryCell)tableview.Root[1][0]).Text, ((EntryCell)tableview.Root[3][0]).Text);
+            }
+        }
+
+        private async void Btn_hel_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var number = ((EntryCell)tableview.Root[1][0]).Text;
+                if (!string.IsNullOrWhiteSpace(number))
+                {
+                    await Launcher.OpenAsync(new Uri("tel:" + number));
+                }
+                else
+                {
+                    await DisplayAlert("Viga", "Palun siseta tel. number", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Viga", $"Tekkis tõrge helistamisel: {ex.Message}", "Ok");
             }
         }
     }
